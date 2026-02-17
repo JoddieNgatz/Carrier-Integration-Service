867647c7b5356ec0ed69ace9e7d2294e72a550fd
Joseph Mureithi <ngatia.joseph@gmail.com>
2026-02-18 00:00:06 +0300

    chore: add tags for swagger, more info in readme

--- src/features/rating/rating.controller.ts
+++ src/features/rating/rating.controller.ts
@@ -3,12 +3,14 @@ import { RatingService } from './rating.service';
 import { AllowAllGuard } from '../../auth/guards/allow-all.guard';
 import { RatingRequest } from './interfaces/rating-request.interface';
 import { RatingResponse } from './interfaces/rating-response.interface';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller()
 export class RatingController {
   constructor(private readonly ratingService: RatingService) {}
-
+  @ApiTags('Rating')
   @Post('rating')
+  @ApiOperation({ summary: 'Get rating for a shipment' })
   //ToDo: Add authentication guard
   @UseGuards(AllowAllGuard) //This is temporarily used to allow all requests to the rating endpoint
   postRating(@Body() request: RatingRequest): Promise<RatingResponse> {
